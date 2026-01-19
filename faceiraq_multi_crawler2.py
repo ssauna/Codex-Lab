d08c23dadce3c8b99635f8b772d7386e98dac535
ssauna <67954409+ssauna@users.noreply.github.com>
2026-01-19 21:36:11 +0300

    Add page load strategy to Chrome options
    Set Chrome page load strategy to 'eager' for faster loading.

--- faceiraq_multi_crawler2.py
+++ faceiraq_multi_crawler2.py
@@ -28,6 +28,7 @@ import re
 import argparse
 from datetime import datetime, timedelta
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
@@ -70,6 +71,7 @@ class FaceIraqMultiCrawler:
         
         # Chrome 옵션 설정
         chrome_options = Options()
+        chrome_options.page_load_strategy = "eager"
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
